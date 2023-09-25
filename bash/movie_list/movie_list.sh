9bc7d77e4ef93cfd380a6435525fa9923bb78ac7
vincent-GL <77963780+Vincent-GL@users.noreply.github.com>
2023-09-25 18:52:34 +0200

    feat: updating movie film script
    you can now remove a file by name from the file

--- bash/movie_list/movie_list.sh
+++ bash/movie_list/movie_list.sh
@@ -4,6 +4,9 @@ set -e
 if [ ! -f movie_list.txt ]; then
     echo "File not found!"
     touch "movie_list.txt"
+    echo "Creating file"
+    echo "..."
+    echo "done !"
 else
 	echo "File found !"
 fi
@@ -15,6 +18,11 @@ if [ "$option" = "add movie" ]; then
     read new_entry
     echo "$new_entry" >> movie_list.txt
     echo "Le film '$new_entry' a été ajouté à la liste."
+else if [ "$option" = "remove movie" ]; then
+    echo "what movie do you want to delete ?"
+    read delete_entry
+    sed -i "/$delete_entry/d" movie_list.txt
+fi
+fi
 echo "current movie list is :"
 cat movie_list.txt
-fi
